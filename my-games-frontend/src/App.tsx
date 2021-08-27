21df0be2e1ef8fe05c0dfeb14fc51cf807031dd4
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2021-08-27 14:48:30 -0300

    build: project configuration

--- my-games-frontend/src/App.tsx
+++ my-games-frontend/src/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <Button variant='contained' color='secondary' onClick={() => console.log('alo')}>Hello</Button>
         <p>
           Good Morning <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
--- my-games-frontend/src/index.tsx
+++ my-games-frontend/src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Palette from './config/theme/Palette';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Palette>
+      <App />
+    </Palette>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
