ebb5d8a725956845ce237b2367fadb01a2898158
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2021-08-28 22:26:04 -0300

    feat: starting blacktheme

--- my-games-frontend/src/config/theme/Palette.tsx
+++ my-games-frontend/src/config/theme/Palette.tsx
@@ -2,25 +2,53 @@ import React, { ReactElement } from 'react';
 import { createTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 
-const theme = createTheme({
+// eslint-disable-next-line no-unused-vars
+const lightTheme = createTheme({
   palette: {
+    // type: 'dark',
     primary: {
       main: '#182647',
+      contrastText: '#FFF',
     },
     secondary: {
       main: '#73EA8C',
+    },
+    background: {
+      default: '#ECEBEB',
+    },
+    text: {
+      primary: '#000',
+      secondary: '#B8B8B8',
+    },
+  },
+});
+
+// eslint-disable-next-line no-unused-vars
+const darkTheme = createTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#182647',
+      contrastText: '#FFF',
+    },
+    secondary: {
+      main: '#73EA8C',
+    },
+    // background: {
+    //   default: '#ECEBEB',
+    // },
+    text: {
+      primary: '#fff',
+      secondary: '#B8B8B8',
     },
   },
 });
 
 interface Props {
-  children: ReactElement
+  children: ReactElement;
 }
-
 const Palette = (props: Props) => (
-  <ThemeProvider theme={theme}>
-    {props.children}
-  </ThemeProvider>
+  <ThemeProvider theme={lightTheme}>{props.children}</ThemeProvider>
 );
 
 export default Palette;
