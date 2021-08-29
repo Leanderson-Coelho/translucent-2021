541762616c434a607ae0726fa65b3ac3cd5f3115
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2021-08-28 22:25:38 -0300

    feat: header componete: name and tc stickers

--- my-games-frontend/src/components/Header/index.tsx
+++ my-games-frontend/src/components/Header/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { AppBar, Toolbar } from '@material-ui/core';
+import './style.css';
 
 const Header = () => (
-  <div>
-    Header Works
-  </div>
+  <AppBar position='fixed' className='header'>
+    <Toolbar classes={{ root: 'headerContainer' }}>
+      <h2 className='headerTitle'>My games</h2>
+      <div className='headerOptionContainer'>
+        <div className='tcSticks'>
+          <div className='sideStick topStick' />
+          <div className='middleStick' />
+          <div className='sideStick bottomStick' />
+        </div>
+      </div>
+    </Toolbar>
+  </AppBar>
 );
 
 export default Header;
