5540cc8624e65298524d94593a686067b56a19f5
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2021-08-29 22:22:31 -0300

    feat: catalog page with cards using gamecard component

--- my-games-frontend/src/pages/Catalog/index.tsx
+++ my-games-frontend/src/pages/Catalog/index.tsx
@@ -1,93 +1,84 @@
-import { Typography, Card } from '@material-ui/core';
 import React from 'react';
 import HeaderContentTemplate from '../../components/HeaderContentTemplate';
 import SearchInput from '../../components/SearchInput';
+import GameCard from '../../components/GameCard';
+import './style.css';
 
-const Catalog = () => (
-  <HeaderContentTemplate>
-    <>
-      <SearchInput />
-      <br />
-      <Card>
-        <Typography color='textPrimary'>
-          For readability we recommend against using more than 80 characters: In
-          code styleguides.
-        </Typography>
-      </Card>
-      <p>
-        For readability we recommend against using more than 80 characters: In
-        code styleguides, maximum line length rules are often set to 100 or 120.
-        However, when humans write code, they don’t strive to reach the maximum
-        number of columns on every line. Developers often use whitespace to
-        break up long lines for readability. In practice, the average line
-        length often ends up well below the maximum. Prettier’s printWidth
-        option does not work the same way. It is not the hard upper allowed line
-        length limit. It is a way to say to Prettier roughly how long you’d like
-        lines to be. Prettier will make both shorter and longer lines, but
-        generally strive to meet the specified printWidth. Remember, computers
-        are dumb. You need to explicitly tell them what to do, while humans can
-        make their own (implicit) judgements, for example on when to break a
-        line. In other words, don’t try to use printWidth as if it was ESLint’s
-        max-len – they’re not the same. max-len just says what the maximum
-        allowed line length is, but not what the generally preferred length is –
-        which is what printWidth specifies.
-      </p>
-      <p>
-        For readability we recommend against using more than 80 characters: In
-        code styleguides, maximum line length rules are often set to 100 or 120.
-        However, when humans write code, they don’t strive to reach the maximum
-        number of columns on every line. Developers often use whitespace to
-        break up long lines for readability. In practice, the average line
-        length often ends up well below the maximum. Prettier’s printWidth
-        option does not work the same way. It is not the hard upper allowed line
-        length limit. It is a way to say to Prettier roughly how long you’d like
-        lines to be. Prettier will make both shorter and longer lines, but
-        generally strive to meet the specified printWidth. Remember, computers
-        are dumb. You need to explicitly tell them what to do, while humans can
-        make their own (implicit) judgements, for example on when to break a
-        line. In other words, don’t try to use printWidth as if it was ESLint’s
-        max-len – they’re not the same. max-len just says what the maximum
-        allowed line length is, but not what the generally preferred length is –
-        which is what printWidth specifies.
-      </p>
-      <p>
-        For readability we recommend against using more than 80 characters: In
-        code styleguides, maximum line length rules are often set to 100 or 120.
-        However, when humans write code, they don’t strive to reach the maximum
-        number of columns on every line. Developers often use whitespace to
-        break up long lines for readability. In practice, the average line
-        length often ends up well below the maximum. Prettier’s printWidth
-        option does not work the same way. It is not the hard upper allowed line
-        length limit. It is a way to say to Prettier roughly how long you’d like
-        lines to be. Prettier will make both shorter and longer lines, but
-        generally strive to meet the specified printWidth. Remember, computers
-        are dumb. You need to explicitly tell them what to do, while humans can
-        make their own (implicit) judgements, for example on when to break a
-        line. In other words, don’t try to use printWidth as if it was ESLint’s
-        max-len – they’re not the same. max-len just says what the maximum
-        allowed line length is, but not what the generally preferred length is –
-        which is what printWidth specifies.
-      </p>
-      <p>
-        For readability we recommend against using more than 80 characters: In
-        code styleguides, maximum line length rules are often set to 100 or 120.
-        However, when humans write code, they don’t strive to reach the maximum
-        number of columns on every line. Developers often use whitespace to
-        break up long lines for readability. In practice, the average line
-        length often ends up well below the maximum. Prettier’s printWidth
-        option does not work the same way. It is not the hard upper allowed line
-        length limit. It is a way to say to Prettier roughly how long you’d like
-        lines to be. Prettier will make both shorter and longer lines, but
-        generally strive to meet the specified printWidth. Remember, computers
-        are dumb. You need to explicitly tell them what to do, while humans can
-        make their own (implicit) judgements, for example on when to break a
-        line. In other words, don’t try to use printWidth as if it was ESLint’s
-        max-len – they’re not the same. max-len just says what the maximum
-        allowed line length is, but not what the generally preferred length is –
-        which is what printWidth specifies.
-      </p>
-    </>
-  </HeaderContentTemplate>
-);
+const Catalog = () => {
+  const games = [
+    {
+      id: 'asd0sa8d0',
+      title: 'Metal Gear Solid 2',
+      year: new Date('01/01/2011'),
+      console: 'PS2',
+      completionDate: new Date(),
+      notes: 'I really liked this game. A masterpiece from Kojima productions.',
+    },
+    {
+      id: 'asd0sa8d1',
+      title: 'Marvel’s Spider-Man',
+      year: new Date('01/01/2018'),
+      console: 'PS4',
+      completionDate: new Date('07/08/2017'),
+      notes:
+        'Using tokens rewarded from objectives, Spider-Man can unlock and craft new gadgets, suits, and Suit Mods',
+    },
+    {
+      id: 'asd0sa8d2',
+      title: 'Super Mario Bros.',
+      year: new Date('09/13/1985'),
+      console: 'NINTENDO',
+      completionDate: null,
+      notes:
+        'In the game, Mario must race through the Mushroom Kingdom and save Princess Toadstool (later Princess Peach) from Bowser.',
+    },
+    {
+      id: 'asd0sa8d3',
+      title: 'Metal Gear Solid 2',
+      year: new Date('01/01/2001'),
+      console: 'PS2',
+      completionDate: new Date(),
+      notes: 'I really liked this game. A masterpiece from Kojima productions.',
+    },
+    {
+      id: 'asd0sa8d4',
+      title: 'Marvel’s Spider-Man',
+      year: new Date('01/01/2018'),
+      console: 'PS4',
+      completionDate: new Date('07/08/2017'),
+      notes:
+        'Using tokens rewarded from objectives, Spider-Man can unlock and craft new gadgets, suits, and Suit Mods',
+    },
+    {
+      id: 'asd0sa8d5',
+      title: 'Super Mario Bros.',
+      year: new Date('09/13/1985'),
+      console: 'NINTENDO',
+      completionDate: null,
+      notes:
+        'In the game, Mario must race through the Mushroom Kingdom and save Princess Toadstool (later Princess Peach) from Bowser.',
+    },
+  ];
+  return (
+    <HeaderContentTemplate>
+      <>
+        <SearchInput />
+        <div className='cardsContainer'>
+          {games.map((game) => (
+            <div key={game.id} className='item'>
+              <GameCard
+                completionDate={game.completionDate}
+                notes={game.notes}
+                title={game.title}
+                year={game.year}
+                console={game.console}
+              />
+            </div>
+          ))}
+        </div>
+      </>
+    </HeaderContentTemplate>
+  );
+};
 
 export default Catalog;
