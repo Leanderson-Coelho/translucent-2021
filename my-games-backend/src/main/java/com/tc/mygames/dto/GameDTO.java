21c89af7939ffd42963ba7b4311469a2675758e9
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2021-09-02 13:40:11 -0300

    feat: GameDTO with validators

--- my-games-backend/src/main/java/com/tc/mygames/dto/GameDTO.java
+++ my-games-backend/src/main/java/com/tc/mygames/dto/GameDTO.java
@@ -3,21 +3,27 @@ package com.tc.mygames.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 public class GameDTO {
 
+    @NotEmpty(message = "error.invalid.title.not.empty")
     private String title;
 
+    @PastOrPresent(message = "error.invalid.completionDate.not.future")
     private LocalDate completionDate;
 
+    @NotEmpty(message = "error.invalid.console.not.empty")
     private String console;
 
+    @NotNull(message = "error.invalid.year.not.null")
     private LocalDate year;
 
     private String notes;
 
+    @NotNull(message = "error.invalid.completed.not.null")
     private Boolean completed;
 }
