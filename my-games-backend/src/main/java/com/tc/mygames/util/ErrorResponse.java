db79726269225e103ac46f3e6db6140599fa58dd
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2021-09-02 13:42:16 -0300

    feat: Util class for buil response errors

--- my-games-backend/src/main/java/com/tc/mygames/util/ErrorResponse.java
+++ my-games-backend/src/main/java/com/tc/mygames/util/ErrorResponse.java
@@ -1,12 +1,23 @@
 package com.tc.mygames.util;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class ErrorResponse {
 
-    public static HashMap<String, String> build(String message) {
-        HashMap<String, String> errorResponse = new HashMap<>();
-        errorResponse.put("error", message);
+    public static HashMap<String, ArrayList<String>> buildOneError(String error) {
+        HashMap<String, ArrayList<String>> errorResponse = new HashMap<>();
+        ArrayList<String> errors = new ArrayList<>();
+        errors.add(error);
+        errorResponse.put("errors", errors);
+        return errorResponse;
+    }
+
+
+    public static HashMap<String, ArrayList<String>> buildManyErrors(ArrayList<String> errors) {
+        HashMap<String, ArrayList<String>> errorResponse = new HashMap<>();
+        errorResponse.put("errors", errors);
         return errorResponse;
     }
 }
