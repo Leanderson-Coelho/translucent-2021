e947b750c45130d84f5dc68194bfb87cbcebbd8b
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2021-09-02 19:34:49 -0300

    feat: Catalog with selector

--- my-games-frontend/src/pages/Catalog/index.tsx
+++ my-games-frontend/src/pages/Catalog/index.tsx
@@ -7,97 +7,22 @@ import { Fab } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import useStyles from './style';
 import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { State } from '../../redux/reducer';
 
 const Catalog = () => {
   const style = useStyles();
   const router = useHistory();
-  const games = [
-    {
-      id: 'asd0sa8d0',
-      title: 'Metal Gear Solid 2',
-      year: new Date('01/01/2011'),
-      console: 'PS2',
-      completionDate: new Date(),
-      notes: 'I really liked this game. A masterpiece from Kojima productions.',
-    },
-    {
-      id: 'asd0sa8d1',
-      title: 'Marvel’s Spider-Man',
-      year: new Date('01/01/2018'),
-      console: 'PS4',
-      completionDate: new Date('07/08/2017'),
-      notes:
-        'Using tokens rewarded from objectives, Spider-Man can unlock and craft new gadgets, suits, and Suit Mods',
-    },
-    {
-      id: 'asd0sa8d2',
-      title: 'Super Mario Bros.',
-      year: new Date('09/13/1985'),
-      console: 'NINTENDO',
-      completionDate: null,
-      notes:
-        'In the game, Mario must race through the Mushroom Kingdom and save Princess Toadstool (later Princess Peach) from Bowser.',
-    },
-    {
-      id: 'asd0sa8d3',
-      title: 'Metal Gear Solid 2',
-      year: new Date('01/01/2001'),
-      console: 'PS2',
-      completionDate: new Date(),
-      notes: 'I really liked this game. A masterpiece from Kojima productions.',
-    },
-    {
-      id: 'asd0sa8d4',
-      title: 'Marvel’s Spider-Man',
-      year: new Date('01/01/2018'),
-      console: 'PS4',
-      completionDate: new Date('07/08/2017'),
-      notes:
-        'Using tokens rewarded from objectives, Spider-Man can unlock and craft new gadgets, suits, and Suit Mods',
-    },
-    {
-      id: 'asd0sa8d5',
-      title: 'Super Mario Bros.',
-      year: new Date('09/13/1985'),
-      console: 'NINTENDO',
-      completionDate: null,
-      notes:
-        'In the game, Mario must race through the Mushroom Kingdom and save Princess Toadstool (later Princess Peach) from Bowser.',
-    },
-    {
-      id: 'asd0sa8d6',
-      title: 'Metal Gear Solid 2',
-      year: new Date('01/01/2001'),
-      console: 'PS2',
-      completionDate: new Date(),
-      notes: 'I really liked this game. A masterpiece from Kojima productions.',
-    },
-    {
-      id: 'asd0sa8d7',
-      title: 'Marvel’s Spider-Man',
-      year: new Date('01/01/2018'),
-      console: 'PS4',
-      completionDate: new Date('07/08/2017'),
-      notes:
-        'Using tokens rewarded from objectives, Spider-Man can unlock and craft new gadgets, suits, and Suit Mods',
-    },
-    {
-      id: 'asd0sa8d8',
-      title: 'Super Mario Bros.',
-      year: new Date('09/13/1985'),
-      console: 'NINTENDO',
-      completionDate: null,
-      notes:
-        'In the game, Mario must race through the Mushroom Kingdom and save Princess Toadstool (later Princess Peach) from Bowser.',
-    },
-  ];
+  const games = useSelector((state: State) => state.games);
+  console.log('Redux State', games);
+
   return (
     <HeaderContentTemplate>
       <>
         <SearchInput />
         <div className='cardsContainer'>
           {games.map((game) => (
-            <div key={game.id} className='item'>
+            <div key={`${game.id}`} className='item'>
               <GameCard
                 completionDate={game.completionDate}
                 notes={game.notes}
