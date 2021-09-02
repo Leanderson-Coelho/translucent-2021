c976871f015f53e9538770c8bd47f06eae64454c
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2021-09-02 13:41:50 -0300

    feat: manual game validation

--- my-games-backend/src/main/java/com/tc/mygames/service/GameService.java
+++ my-games-backend/src/main/java/com/tc/mygames/service/GameService.java
@@ -2,11 +2,12 @@ package com.tc.mygames.service;
 
 import com.tc.mygames.domain.Game;
 import com.tc.mygames.dto.GameDTO;
-import com.tc.mygames.exception.GameTitleAlreadyExistsException;
+import com.tc.mygames.exception.*;
 import com.tc.mygames.repository.GameRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -16,24 +17,41 @@ public class GameService {
 
     private GameRepository gameRepository;
 
-    public Game save(Game game) throws GameTitleAlreadyExistsException {
+    public Game save(GameDTO gameDTO) throws GameTitleAlreadyExistsException, FailOperationException, MinimumGameYearLimitException, MaximumGameTitleLimitException {
+        try {
+            Game alreadyExists = gameRepository.findByTitle(gameDTO.getTitle());
+            if (alreadyExists != null) {
+                Logger.getLogger("TC.MYGAMES").info("Already exists one game with this title: " + alreadyExists.getTitle());
+                throw new GameTitleAlreadyExistsException();
+            }
 
-        Game alreadyExists = gameRepository.findByTitle(game.getTitle());
-        if (alreadyExists != null) {
-            Logger.getLogger("TC.MYGAMES").info("Already exists one game with this title: " + alreadyExists.getTitle());
-            throw new GameTitleAlreadyExistsException();
+            if (gameDTO.getYear().isBefore(LocalDate.of(1970, 01, 01))) {
+                Logger.getLogger("TC.MYGAMES").info("Minimum limit for game year reached");
+                throw new MinimumGameYearLimitException();
+            }
+
+            if (gameDTO.getTitle().length() > 100) {
+                Logger.getLogger("TC.MYGAMES").info("Maximum game title limit reached");
+                throw new MaximumGameTitleLimitException();
+            }
+
+            Game gameDocument = new Game(gameDTO.getTitle(), gameDTO.getCompletionDate(), gameDTO.getConsole(), gameDTO.getYear(), gameDTO.getNotes(), gameDTO.getCompleted());
+
+            return gameRepository.save(gameDocument);
+        } catch (GameTitleAlreadyExistsException | MinimumGameYearLimitException | MaximumGameTitleLimitException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new FailOperationException(OperationType.SAVE.getType());
         }
 
-        Game gameDocument = new Game(game.getTitle(), game.getCompletionDate(), game.getConsole(),
-                game.getYear(), game.getNotes(), game.getCompleted());
-
-        System.out.println("game:\n" + game.toString());
-
-        return gameRepository.save(gameDocument);
     }
 
-    public List<Game> findAll() {
-        return gameRepository.findAll();
+    public List<Game> findAll() throws FailOperationException {
+        try {
+            return gameRepository.findAll();
+        } catch (Exception e) {
+            throw new FailOperationException(OperationType.FIND_ALL.getType());
+        }
     }
 
 }
