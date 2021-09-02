829868f6e3eb5498a002ac211830dac24fe65696
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2021-09-02 07:44:19 -0300

    feat: Game domain 'Document'

---
+++ my-games-backend/src/main/java/com/tc/mygames/domain/Game.java
@@ -0,0 +1,47 @@
+package com.tc.mygames.domain;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@Getter
+@Setter
+@Document
+public class Game implements Serializable {
+
+    @Id
+    private UUID id;
+
+    @NotEmpty
+    @NotNull
+    private String title;
+
+    @Past(message = "error.invalid.date.past.tc")
+    private LocalDate completionDate;
+
+    private String console;
+
+    private LocalDate year;
+
+    private String notes;
+
+    private Boolean completed;
+
+    public Game(String title, LocalDate completionDate, String console, LocalDate year, String notes, Boolean completed) {
+        this.id = UUID.randomUUID();
+        this.title = title;
+        this.completionDate = completionDate;
+        this.console = console;
+        this.year = year;
+        this.notes = notes;
+        this.completed = completed;
+    }
+}
