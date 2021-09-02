c9da68b1c78e253112fad615693eed50695ec699
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2021-09-02 07:42:30 -0300

    build: backend initial tests

---
+++ my-games-backend/src/test/java/com/tc/mygames/MyGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tc.mygames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
