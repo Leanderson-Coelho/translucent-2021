605f9a662cdbcfe052317761d5e1df83bed3fe1e
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2021-09-02 07:45:26 -0300

    feat: Initial package exception

---
+++ my-games-backend/src/main/java/com/tc/mygames/exception/GameTitleAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.tc.mygames.exception;
+
+public class GameTitleAlreadyExistsException extends Exception {
+
+    public GameTitleAlreadyExistsException() {
+        super("error.form.gameTitleAlreadyExists");
+    }
+
+}
