cc8eae0c6f9953d51ae89c3a811af4de8f94f7d3
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2021-09-02 07:46:42 -0300

    feat: main class spring

---
+++ my-games-backend/src/main/java/com/tc/mygames/MyGamesApplication.java
@@ -0,0 +1,13 @@
+package com.tc.mygames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGamesApplication.class, args);
+    }
+
+}
