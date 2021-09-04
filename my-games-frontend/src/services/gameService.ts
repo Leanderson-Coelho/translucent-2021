16503b714ba5706f4e442f9c05fe87f5a32e2d4c
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2021-09-04 15:17:19 -0300

    feat: creating game service to use in saga function

---
+++ my-games-frontend/src/services/gameService.ts
@@ -0,0 +1,18 @@
+import { DateTime } from 'luxon';
+import axios from 'axios';
+import Game from '../model/Game';
+import { API_BASE_PATH } from '../config/apiConfig';
+
+export default {
+  async fetchGames(): Promise<Game[]> {
+    const response = await axios.get(`${API_BASE_PATH}/game`);
+    if (response.data) {
+      return response.data.map((game: any) => ({
+        ...game,
+        year: DateTime.fromISO(game.year),
+        completionDate: DateTime.fromISO(game.completionDate),
+      }));
+    }
+    return [];
+  },
+};
